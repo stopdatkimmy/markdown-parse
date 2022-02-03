4d7ad650d8494caa8c68e7abc4c7f7b8a5343faf
BrianLiu1319 <brianliu1337@yahoo.com>
2022-02-02 18:13:59 -0800

    aa

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,13 +6,15 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void tester() throws IOException {
-        List<String> expected = List.of("[]","[]","[]","[https://something.com, some-page.html],[]");
-        List<String> list = List.of("empty-file.md", "empty-link.md", "image-file.md", "test-file.md,anotherfile.md");
+        List<String> expected = List.of("[]","[]","[]","[https://something.com, some-page.html]","[]");
+        List<String> list = List.of("empty-file.md", "empty-link.md", "image-file.md", "test-file.md","anotherfile.md");
         for (int i=0; i<list.size(); i++) {
             Path fileName = Path.of(list.get(i));
             String contents = Files.readString(fileName);
