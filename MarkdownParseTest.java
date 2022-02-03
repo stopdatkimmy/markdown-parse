7c54e48f47dd20c63a1522d8c622a1f7548a50b8
Merric <merrickqiu@gmail.com>
2022-02-02 17:36:09 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -11,7 +11,7 @@ import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void tester() throws IOException {
-        List<String> expected = List.of("","","","[https://something.com, some-page.html]");
+        List<String> expected = List.of("[]","[]","[]","[https://something.com, some-page.html]");
         List<String> list = List.of("empty-file.md", "empty-link.md", "image-file.md", "test-file.md");
         for (int i=0; i<list.size(); i++) {
             Path fileName = Path.of(list.get(i));
